48c8705e2ec7de13e8b65a3a43409851f747aaaa
Aelwyhn <artem.parfenov@indriver.com>
2023-10-14 21:02:23 +0600

    update compileSdk to 34; update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.aelwyhn.cosby"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.aelwyhn.cosby"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
