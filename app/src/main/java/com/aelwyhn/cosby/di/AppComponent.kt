2067bd7cff2970826a0edd773fa5ef77b716593a
Aelwyhn <artem.parfenov@indriver.com>
2023-10-16 21:46:39 +0600

    app: adjust di

--- app/src/main/java/com/aelwyhn/cosby/di/AppComponent.kt
+++ app/src/main/java/com/aelwyhn/cosby/di/AppComponent.kt
@@ -1,19 +1,19 @@
 package com.aelwyhn.cosby.di
 
 import com.aelwyhn.cosby.CosbyApplication
+import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(
     modules = [AppModule::class],
-    dependencies = [CosbyApplication::class]
 )
 interface AppComponent : CoreProvider {
 
     @Component.Factory
     interface Factory {
-        fun create(app: CosbyApplication): AppComponent
+        fun create(@BindsInstance app: CosbyApplication): AppComponent
     }
 
     fun inject(app: CosbyApplication)
--- app/src/main/java/com/aelwyhn/cosby/di/AppModule.kt
+++ app/src/main/java/com/aelwyhn/cosby/di/AppModule.kt
@@ -4,14 +4,12 @@ import android.content.Context
 import com.aelwyhn.cosby.CosbyApplication
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 
 @Module
 class AppModule {
 
-    @Singleton
     @Provides
     fun provideContext(app: CosbyApplication): Context {
-        return app
+        return app.applicationContext
     }
 }
